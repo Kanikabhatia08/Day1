51045a646d633c138c1246f927500659f3c68cff
Kanikabhatia08 <kanikabhatia08sep@gmail.com>
2023-05-24 01:47:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    basic:["Basic"],
+    pfd:["Playfair Display"],
+    
+  }
+};
